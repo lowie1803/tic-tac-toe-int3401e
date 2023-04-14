e93ecbd7b3c4cee112d7cfed6c499300c9552300
lowie1803 <dung.totuan01@gmail.com>
2023-04-15 00:46:05 +0700

    Add config and fake.db

--- README.md
+++ README.md
@@ -1,5 +1,58 @@
 # Getting Started with Create React App
 
+## To boot up FE server
+
+`yarn start`
+
+## To boot up fake api server
+`yarn json-server --watch fake-db/db.json --port 3001`
+
+## Notes
+Can create `config.yml` to config URL and PASSPHRASE
+
+### Sample js post request
+```js
+fetch('http://localhost:3001/game_states', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ your_json_here })
+})
+  .then(response => response.json())
+  .then(data => console.log(data));
+```
+
+```python
+import requests
+url = 'http://localhost:3001/game_states'
+data = {} # your data
+response = requests.post(url, json=data)
+```
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### default react readme.md
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import yaml from 'js-yaml';
 
 function App() {
+  const [gameStates, setGameStates] = useState({
+    match_id: 123,
+    status: null,
+    size: 0,
+    board: [],
+    time1: 100,
+    time2: 100,
+    team1_id: "team1",
+    team2_id: "team2",	
+    turn: 1,
+    score1: 0,
+    score2: 1
+  });
+
+  const [config, setConfig] = useState({
+    server_url: "http://localhost",
+    passphrase: "something cool",
+    frontend_port: 3000,
+    fakedb_port: 3001
+  });
+
+  useEffect(() => {
+    fetch('/config.yml')
+      .then(response => response.text())
+      .then(yamlText => yaml.load(yamlText))
+      .then(data => setConfig(data));
+  }, []);
+
+
+  useEffect(() => {
+    fetch(`${config.server_url || "http://localhost"}:${config.fakedb_port || 3001}/game_states`)
+      .then((response) => response.json())
+      .then((data) => setGameStates(data));
+  }, []);
+
+
+  console.log(gameStates);
   return (
     <div className="App">
       <header className="App-header">
